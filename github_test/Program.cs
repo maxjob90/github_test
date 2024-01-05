81447d3c9f5c076538c035b6448450de033c82bf
Calculator <maxjob90@mail.ru>
2024-01-05 16:31:33 +0300

    feat: add difference of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -1,6 +1,36 @@
-﻿public class Program
+﻿public class Programm
 {
     static void Main(string[] args)
     {
+        while (true)
+        {
+            Console.WriteLine("Введите первое число:");
+            var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Выберите команду: \n" +
+                              $"1. Сложить числа \n" +
+                              $"2. Вычесть числа \n");
+
+            var choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                {
+                    var sum = firstNumber + secondNumber;
+                    Console.WriteLine("Результат сложения " + sum);
+                    break;
+                }
+                case 2:
+                {
+                    var difference = firstNumber - secondNumber;
+                    Console.WriteLine("Результат вычитания " + difference);
+                    break;
+                }
+            }
+        }
     }
 }
